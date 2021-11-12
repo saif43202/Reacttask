5b169f74261a91cd885cde865de0467580620715
saif43202 <80760576+saif43202@users.noreply.github.com>
2021-11-12 15:20:22 +0530

    Add files via upload

---
+++ Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Hello.js';
+import user from './user.js';
+
+class Hello extends React.Component {
+	render(){
+		return(
+			<div>
+				<h1>this is hello class component</h1>
+			</div>
+		);
+	}
+}
+export default Hello;
